109cf6f75e471b6fb27782cf54b94c5571e91c63
Chris Boydstun <chrisboydstun@gmail.com>
2022-01-15 17:52:24 -0600

    form working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import Form from './components/Form'
+
 export default function App() {
     return (
         <div>
-            hey from React
+            <Form />
         </div>
     )
 }
