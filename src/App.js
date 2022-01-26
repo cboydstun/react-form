8162833159e578017b1a5110150e3b3d15e11f75
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2022-01-25 21:37:51 -0600

    rendering data

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React from 'react'
 import Form from './components/Form'
 
 export default function App() {
-    return (
-        <div>
-            <Form />
-        </div>
-    )
+  return (
+    <div className="app">
+      <Form />
+    </div>
+  )
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,101 +1,136 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 export default function Form() {
+  // States for registration
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [data, setData] = useState([])
 
-// States for registration
-const [name, setName] = useState('');
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
+  // States for checking the errors
+  const [submitted, setSubmitted] = useState(false)
+  const [error, setError] = useState(false)
 
-// States for checking the errors
-const [submitted, setSubmitted] = useState(false);
-const [error, setError] = useState(false);
+  // Handling the name change
+  const handleName = (e) => {
+    setName(e.target.value)
+    setSubmitted(false)
+  }
 
-// Handling the name change
-const handleName = (e) => {
-	setName(e.target.value);
-	setSubmitted(false);
-};
+  // Handling the email change
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+    setSubmitted(false)
+  }
 
-// Handling the email change
-const handleEmail = (e) => {
-	setEmail(e.target.value);
-	setSubmitted(false);
-};
+  // Handling the password change
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+    setSubmitted(false)
+  }
 
-// Handling the password change
-const handlePassword = (e) => {
-	setPassword(e.target.value);
-	setSubmitted(false);
-};
+  // Handling the form submission
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
-// Handling the form submission
-const handleSubmit = (e) => {
-	e.preventDefault();
-	if (name === '' || email === '' || password === '') {
-	setError(true);
-	} else {
-	setSubmitted(true);
-	setError(false);
-	}
-};
+    if (name === '' || email === '' || password === '') {
+      setError(true)
+    } else {
+      setSubmitted(true)
+      setError(false)
 
-// Showing success message
-const successMessage = () => {
-	return (
-	<div
-		className="success"
-		style={{
-		display: submitted ? '' : 'none',
-		}}>
-		<h1>User {name} successfully registered!!</h1>
-	</div>
-	);
-};
+      setData([...data, { name, email, password }])
+    }
 
-// Showing error message if error is true
-const errorMessage = () => {
-	return (
-	<div
-		className="error"
-		style={{
-		display: error ? '' : 'none',
-		}}>
-		<h1>Please enter all the fields</h1>
-	</div>
-	);
-};
+    console.log(data)
+  }
 
-return (
-	<div className="form">
-	<div>
-		<h1>User Registration</h1>
-	</div>
+  // Showing success message
+  const successMessage = () => {
+    return (
+      <div
+        className="success"
+        style={{
+          display: submitted ? '' : 'none',
+        }}
+      >
+        <h1>User {name} successfully registered!!</h1>
+      </div>
+    )
+  }
 
-	{/* Calling to the methods */}
-	<div className="messages">
-		{errorMessage()}
-		{successMessage()}
-	</div>
+  // Showing error message if error is true
+  const errorMessage = () => {
+    return (
+      <div
+        className="error"
+        style={{
+          display: error ? '' : 'none',
+        }}
+      >
+        <h1>Please enter all the fields</h1>
+      </div>
+    )
+  }
 
-	<form>
-		{/* Labels and inputs for form data */}
-		<label className="label">Name</label>
-		<input onChange={handleName} className="input"
-		value={name} type="text" />
+  return (
+    <div className="container">
+      <div>
+        <h1>User Registration</h1>
+      </div>
 
-		<label className="label">Email</label>
-		<input onChange={handleEmail} className="input"
-		value={email} type="email" />
+      {/* Calling to the methods */}
+      <div className="messages">
+        {errorMessage()}
+        {successMessage()}
+      </div>
 
-		<label className="label">Password</label>
-		<input onChange={handlePassword} className="input"
-		value={password} type="password" />
+      <form>
+        {/* Labels and inputs for form data */}
+        <label className="label">Name</label>
+        <input
+          onChange={handleName}
+          className="input"
+          value={name}
+          type="text"
+        />
 
-		<button onClick={handleSubmit} className="btn" type="submit">
-		Submit
-		</button>
-	</form>
-	</div>
-);
+        <label className="label">Email</label>
+        <input
+          onChange={handleEmail}
+          className="input"
+          value={email}
+          type="email"
+        />
+
+        <label className="label">Password</label>
+        <input
+          onChange={handlePassword}
+          className="input"
+          value={password}
+          type="password"
+        />
+
+        <button onClick={handleSubmit} className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+
+      {/* Showing the data */}
+      <div className="data">
+        <h1>Data</h1>
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>
+              <p>
+                Name: {item.name} {index}
+              </p>
+              <p>Email: {item.email}</p>
+              <p>Password: {item.password}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
